5d30bdb17d4295323dd7cb41bb9b2b62b2bb5aa1
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-08-29 19:57:53 -0300

    Comentários em JAVA

--- testes em java/src/App.java
+++ testes em java/src/App.java
@@ -1,15 +1,16 @@
-public class App {
+public class App {//Agora eu sei usar os comentários no JAVA (Diferente de outra linguagem que demorei para aprender...)
     public static void main(String[] args) {
       System.out.println("Olá mundo, primeira linha de código de futuras muitas");
       System.out.println(5*4*6*3/5/3*58*5*785-496-496+5-9694*96*795*7+396597/5*8*5*8/59+8);
       System.out.println(true);
       System.out.println(false);
       System.out.println(5>3);
+      /* 
       System.out.println(0==909);
       System.out.println(56*56>2*2*2*2*2*2*2*2*2*2);
       System.out.println("Agora eu sei o básico de JAVA"); 
       System.out.print(3*3+2*2*2*2*2*2*2*2+0.0000058);
       System.out.print(6*6/6*6/6/6*6*5/76*4/6*4/6*4/65*5/6*5/67*5/6*0);
-
+*/
     }
   }
