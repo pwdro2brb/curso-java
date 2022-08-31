beb86559877e76738337b1b76c6edc83373e16eb
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-08-30 21:40:14 -0300

    Exercicio JAVA 1.0

--- testes em java/src/App.java
+++ testes em java/src/App.java
@@ -1,14 +1,9 @@
 public class App {
     public static void main(String[] args) {
-         ola();
-         System.out.println("Ufa eu acho ????"); 
+         
+         novo.ola();
       }
+    }
 
-    public static void ola() {
-
-      System.out.println("Olá mundo, segunda linha de código de futuras muitas");
-      System.out.println(5*4*6*3/5/3*58*5*785-496-496+5-9694*96*795*7+396597/5*8*5*8/59+8);
-
-    }  
       
-  }
+  
--- testes em java/src/novo.java
+++ testes em java/src/novo.java
@@ -1,7 +1,13 @@
 public class novo {
     
     public static void main(String[] args) {
-        App.ola();
+        novo.ola();
         
     }
+    public static void ola() {
+
+        System.out.println("Pedro Henrique Soares Silva");
+        System.out.println(10*2-100+100-4);
+  
+      }  
 }
