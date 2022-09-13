3b184e6380c85bbcd3fceb6fdf82624185cb6d33
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-09-12 21:39:28 -0300

    const em JAVA

--- testes em java/src/novo.java
+++ testes em java/src/novo.java
@@ -5,9 +5,14 @@ public class novo {
         
     }
     public static void ola() {
-
-        System.out.println("Pedro Henrique Soares Silva");
-        System.out.println(10*2-100+100-4);
+// final torna uma variável em uma constante, na qual o valor dela não pode ser mais alterado.  
+      final String nome = "Andressa";
+      final String nascimento = "18/11/2032";
+      final String genero = "F";
+      final String CPF = "874029382332"; 
+        System.out.println();
+        System.out.println("O seu nome é "+ nome +" Você nasceu no dia " + nascimento + " Você é do gênero "+genero+" Com o cpf "+CPF);
+        System.out.println();
   
       }  
 }
